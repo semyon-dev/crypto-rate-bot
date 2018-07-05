6f8481f56e474c0c65ecf9d5806ff53cfad22335
SemyonNovikov <semennovikov1@yandex.ru>
2018-07-05 20:53:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,41 +8,54 @@ port = int(os.environ.get('PORT', 5000))
 token = "TOKEN"
 bot = telebot.TeleBot(token)
 
-
-con = psycopg2.connect("host=''")
+con = psycopg2.connect("host='your host'dbname='******'user='*******'password='your password'")
 cursor = con.cursor()
 
-#cursor.execute("CREATE TABLE users (id int, mode varchar(10) )")
-
-#cursor.execute("DROP TABLE <name of table>") # удалить базу данных
+#cursor.execute("DROP TABLE users") # удалить базу данных
+#con.commit()
+#cursor.execute("CREATE TABLE users (id varchar(60), mode varchar(20) )")
+#con.commit()
 
 @bot.message_handler(func=lambda msg: True)
 def greeting(message):
 
-    cursor.execute('SELECT * FROM users WHERE id=%s', (message.chat.id,))
-    user = (cursor.fetchone())
+    try:
+        chat_id = str(message.chat.id)
+        cursor.execute('SELECT * FROM users WHERE id=%s', (chat_id,))
+        user = (cursor.fetchone())
+    except:
+        msg = 'error'
 
     if user == None:
 
-        cursor.execute("INSERT INTO users VALUES(%s,%s)",(message.chat.id,'own'))
+        cursor.execute("INSERT INTO users VALUES(%s,%s)",(chat_id,'own'))
         con.commit()
 
     try:
+
         text = message.text  # get user message
 
         if '/change_mode_chat' in text:
 
-            cursor.execute('''UPDATE users SET mode = %s WHERE id = %s''', ('chat', message.chat.id))
+            cursor.execute('''UPDATE users SET mode = %s WHERE id = %s''', ('chat',chat_id,))
             con.commit()
 
-            s='режим успешно изменен'
+            s = 'mode was changed'
 
         elif '/change_mode_own' in text:
 
-            cursor.execute('''UPDATE users SET mode = %s WHERE id = %s''', ('own', message.chat.id ))
+            cursor.execute('''UPDATE users SET mode = %s WHERE id = %s''', ('own',chat_id))
             con.commit()
 
-            s = 'Режим успешно изменен'
+            s = 'mode was changed'
+
+        elif text=='/start':
+
+            s = 'Hello!\nExample of commands: btc usd, btc rur, zec usd'
+
+        elif text == '/help':
+
+            s = 'Hello!Example of commands: btc usd, btc rur, zec usd\nThis rates are from WEX.Change bot mode for chat: /change_mode_chat (and for own /change_bot_own)'
 
         else:
             pair = text.replace(" ", "_")  # replace to get pair
@@ -51,16 +64,21 @@ def greeting(message):
             response = requests.get(url)
             list = (response.json()[pair])  # get list
             content = dict(list)  # convert to dictionary
-            s = "Курс " + text + " - " + str(content["last"])  # print ["last"] price
+            s = text + " - " + str(content["last"])  # print ["last"] price
 
-    except KeyError:
+    except:
 
-        if user[1] == 'own':
-            s = "Ошибка!Вы неправильно указали пару!"
-        else:
-            s=None
+        try:
 
-    if s!=None:
+            if user[1] == 'own':
+                s = "Not valid pair( /help )"
+            else:
+                s = None
+
+        except:
+            s = None
+
+    if s != None:
 
         bot.send_message(message.chat.id, s)
 
