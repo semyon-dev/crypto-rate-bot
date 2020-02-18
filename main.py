2e654f0dd0b231b8ce429cfc0b2b026dea1a50f9
Semyon <semennovikov1@yandex.ru>
2020-02-19 00:11:41 +0300

    project refactoring

--- main.py
+++ main.py
@@ -1,60 +1,18 @@
+import os
 import time
 import traceback
-import telebot
+
 import requests
-import os
+import telebot
+
+import config
 
 port = int(os.environ.get('PORT', 5000))
 
-token = "605519378:AAEd0WOYiatGI-Nrf9vK2DLgJdFVD2NpMZc"
-bot = telebot.TeleBot(token)
+bot = telebot.TeleBot(config.token)
 
-donate = '''
-You can support creator:
-Bitcoin:
-16gzM2uGF8WyfamRrwNQdFCpKBe8b7zvw9
-Ethereum:
-0x962f70bDb0B9Dccd86249385408359fb5136b27D
-Litecoin:
-LPvJjwK8hVumbDQ9ijGPS1AKSDqFy169fT
-Dash:
-XnNjRFm3XF5qfj1TPRhdcdGXVEDi5ZNhJV
-Zcash:
-t1HxXL9NGxmUsXDq1SBRmpXUMc4twbok4j1
-'''
-
-help = '''
-✅ This bot can give you crypto rates
-✅ You can add bot to chat
-Commands for example: btc usd, zec btc, etc
-—————————————
-If you need your own bot please contact me:
-Creator: @semyon_bitcoin
-—————————————
-Bitcoin donate:
-16gzM2uGF8WyfamRrwNQdFCpKBe8b7zvw9
-Litecoin donate:
-LPvJjwK8hVumbDQ9ijGPS1AKSDqFy169fT
-More donates - /donate
-'''
-
-start = '''
-✅ This bot can give you crypto rates
-✅ Need help? /help
-✅ You can add bot to chat
-Commands for example: btc usd, zec btc, etc
-—————————————
-If you need your own bot please contact me:
-Creator: @semyon_bitcoin
-—————————————
-Bitcoin donate:
-16gzM2uGF8WyfamRrwNQdFCpKBe8b7zvw9
-Litecoin donate:
-LPvJjwK8hVumbDQ9ijGPS1AKSDqFy169fT
-'''
 
 def human_redable(n):
-
     sepdec = '.'
     n = str(n)
     if sepdec in n:
@@ -71,56 +29,57 @@ def human_redable(n):
 
     return out
 
-def get_rate_wex(text):
 
+def get_rate_wex(text):
     try:
         pair_wex = text.replace(' ', '_').lower()
         url = 'https://wex.nz/api/3/ticker/' + pair_wex  # send json request
         response = requests.get(url)
         list = (response.json()[pair_wex])  # get list
         content = dict(list)  # convert to dictionary
-        rate = 'Wex: ' + text + ' - ' + str(round(float(content['last']),5))
+        rate = 'Wex: ' + text + ' - ' + str(round(float(content['last']), 5))
     except:
         rate = 'Wex: not found'
 
     return rate
+
 
 def get_rate_binance(text):
     try:
         pair_binance = text.replace(' ', '').upper()
 
         if 'USD' in pair_binance:
-            pair_binance = pair_binance.replace('USD','USDT')
+            pair_binance = pair_binance.replace('USD', 'USDT')
 
         url = 'https://api.binance.com/api/v3/ticker/price?symbol=' + pair_binance  # send json request
         response = requests.get(url)
         list = (response.json())  # get list
         content = dict(list)  # convert to dictionary
-        rate = 'Binance: ' + text + ' - ' + str(round(float(content['price']),5))
+        rate = 'Binance: ' + text + ' - ' + str(round(float(content['price']), 5))
     except:
-        rate='Binance: not found'
+        rate = 'Binance: not found'
 
     return rate
 
+
 @bot.message_handler(func=lambda msg: True)
 def greeting(message):
-
     try:
         text = message.text  # get user message
 
         if text == '/start':
 
-            response = start
+            response = text.start
 
-        elif text == '/help' or text== '/help@crypto_costs_bot':
+        elif text == '/help' or text == '/help@crypto_costs_bot':
 
             response = help
 
-        elif text == '/donate' or text=='/donate@crypto_costs_bot':
+        elif text == '/donate' or text == '/donate@crypto_costs_bot':
 
-            response =  donate
+            response = text.donate
 
-        elif text=='/marketcap' or text=='/marketcap@crypto_costs_bot':
+        elif text == '/marketcap' or text == '/marketcap@crypto_costs_bot':
 
             url = 'https://api.coinmarketcap.com/v2/global/'
             response = requests.get(url)
@@ -158,7 +117,7 @@ def greeting(message):
             response = 'Number of cryptocurrencies: ' + str(allcrypto)
 
 
-        elif text=='/btcdomimation' or text=='/btcdomimation@crypto_costs_bot':
+        elif text == '/btcdomimation' or text == '/btcdomimation@crypto_costs_bot':
 
             url = 'https://api.coinmarketcap.com/v2/global/'
             response = requests.get(url)
@@ -170,12 +129,12 @@ def greeting(message):
             response = 'Domination of bitcoin: ' + str(btcdom) + '%'
 
         else:
-            response=''
-            response +=  get_rate_wex(text)
+            response = ''
+            response += get_rate_wex(text)
             response += '\n'
             response += get_rate_binance(text)
 
-            if response.count('not found')==2:
+            if response.count('not found') == 2:
                 int('error')
 
         bot.send_message(message.chat.id, response)
@@ -188,6 +147,7 @@ def greeting(message):
         if message.chat.type != 'group' and message.chat.type != 'supergroup':
             response = 'not valid par or command'
             bot.send_message(message.chat.id, response)
+
 
 while True:
     try:
---
+++ text.py
@@ -0,0 +1,43 @@
+donate = '''
+You can support creator:
+Bitcoin:
+16gzM2uGF8WyfamRrwNQdFCpKBe8b7zvw9
+Ethereum:
+0x962f70bDb0B9Dccd86249385408359fb5136b27D
+Litecoin:
+LPvJjwK8hVumbDQ9ijGPS1AKSDqFy169fT
+Dash:
+XnNjRFm3XF5qfj1TPRhdcdGXVEDi5ZNhJV
+Zcash:
+t1HxXL9NGxmUsXDq1SBRmpXUMc4twbok4j1
+'''
+
+help = '''
+✅ This bot can give you crypto rates
+✅ You can add bot to chat
+Commands for example: btc usd, zec btc, etc
+—————————————
+If you need your own bot please contact me:
+Creator: @semyon_dev
+—————————————
+Bitcoin donate:
+16gzM2uGF8WyfamRrwNQdFCpKBe8b7zvw9
+Litecoin donate:
+LPvJjwK8hVumbDQ9ijGPS1AKSDqFy169fT
+More donates - /donate
+'''
+
+start = '''
+✅ This bot can give you crypto rates
+✅ Need help? /help
+✅ You can add bot to chat
+Commands for example: btc usd, zec btc, etc
+—————————————
+If you need your own bot please contact me:
+Creator: @semyon_dev
+—————————————
+Bitcoin donate:
+16gzM2uGF8WyfamRrwNQdFCpKBe8b7zvw9
+Litecoin donate:
+LPvJjwK8hVumbDQ9ijGPS1AKSDqFy169fT
+'''
